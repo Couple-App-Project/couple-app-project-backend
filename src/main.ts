d41c3f47e5058169bf527999e82426ebefd3e771
Donghyeon Seo <92674373+donghyeon23@users.noreply.github.com>
2022-11-22 23:48:06 +0900

    :seedling: [Add]: 유저 모듈 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { SWAGGER_CONFIG } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, SWAGGER_CONFIG);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
