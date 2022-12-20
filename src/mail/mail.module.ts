dc8aa71a87df2f8f47a6fa7a88b563beaf582ab9
Donghyeon Seo <92674373+donghyeon23@users.noreply.github.com>
2022-12-21 01:15:42 +0900

    [Add]: process.env 에서 configSerivce로 변경
    환경별로 관리하기 위해 변경했습니다

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,33 +1,47 @@
-import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
+import { MailerModule, MailerOptions } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 import { MailService } from './mail.service';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        // ignoreTLS: true,
-        secure: false,
-        auth: {
-          user: process.env.EMAIL_AUTH_USER,
-          pass: process.env.EMAIL_AUTH_PASSWORD,
-        },
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<MailerOptions> => {
+        const host = configService.get('EMAIL_HOST');
+        const port = configService.get('EMAIL_PORT');
+        const user = configService.get('EMAIL_AUTH_USER');
+        const pass = configService.get('EMAIL_AUTH_PASSWORD');
+        return {
+          transport: {
+            host,
+            port,
+            // ignoreTLS: true,
+            secure: false,
+            auth: {
+              user,
+              pass,
+            },
+          },
+          defaults: {
+            from: '"CoupleApp" <no-reply@gmail.com>',
+          },
+          template: {
+            dir: join(__dirname, 'templates'),
+            adapter: new EjsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
       },
-      defaults: {
-        from: '"CoupleApp" <no-reply@gmail.com>',
-      },
-      template: {
-        dir: join(__dirname, 'templates'),
-        adapter: new EjsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -6,8 +7,10 @@ import { SWAGGER_CONFIG } from './config/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
   const document = SwaggerModule.createDocument(app, SWAGGER_CONFIG);
   SwaggerModule.setup('api/docs', app, document);
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
