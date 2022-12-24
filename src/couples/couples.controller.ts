93e4ca5ac81177ef7872c1df0050a4b6b9a0fcb1
Jeongho Kim <43710866+dev-logan@users.noreply.github.com>
2022-12-24 18:12:21 +0900

    [feat]: 커플 정보 입력 및 수정 API

--- src/couples/couples.controller.ts
+++ src/couples/couples.controller.ts
@@ -3,6 +3,7 @@ import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ConnectCoupleDto } from './dto/connect-couple.dto';
+import { AddCoupleInformationDto } from './dto/add-couple-information.dto';
 
 @Controller('couples')
 export class CouplesController {
@@ -18,6 +19,7 @@ export class CouplesController {
     if (me.coupleId) {
       return { message: '당신은 이미 커플로 연결된 상태입니다.' };
     }
+
     const opponent = await this.prisma.user.findFirst({
       where: { inviteCode: connectCoupleDto.inviteCode },
     });
@@ -27,6 +29,7 @@ export class CouplesController {
     if (opponent.id === req.user.userId) {
       return { message: '다른 사람의 코드를 입력하십시오.' };
     }
+
     const newCouple = await this.prisma.couple.create({ data: {} });
     await this.prisma.user.update({
       where: {
@@ -44,6 +47,55 @@ export class CouplesController {
         coupleId: newCouple.id,
       },
     });
+
     return { message: '커플 연결이 완료되었습니다.' };
+  }
+
+  @Post('info')
+  @ApiOperation({ summary: '커플 정보 입력 및 수정' })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async addCoupleInformation(
+    @Req() req,
+    @Body() addCoupleInformationDto: AddCoupleInformationDto,
+  ) {
+    const { nickname, ...coupleInformation } = addCoupleInformationDto;
+
+    // 나의 커플 id 알기
+    const me = await this.prisma.user.findFirst({
+      where: {
+        id: req.user.userId,
+      },
+    });
+
+    // 커플 id로 검색하여 상대방 데이터에 접근
+    const meAndOpponent = await this.prisma.user.findMany({
+      where: {
+        coupleId: me.coupleId,
+      },
+    });
+    const opponent = meAndOpponent.filter(
+      (value) => value.id !== req.user.userId,
+    )[0];
+
+    // nickname 변경
+    await this.prisma.user.update({
+      where: {
+        id: opponent.id,
+      },
+      data: {
+        nickname: nickname,
+      },
+    });
+
+    // 커플 정보 업데이트
+    await this.prisma.couple.update({
+      where: {
+        id: me.coupleId,
+      },
+      data: coupleInformation,
+    });
+
+    return { message: '커플 정보 입력 및 수정 완료' };
   }
 }
