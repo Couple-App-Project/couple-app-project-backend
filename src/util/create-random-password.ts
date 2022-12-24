5e89b19b4a19e210cb40fe4e0eb30372f221a561
Jeongho Kim <43710866+dev-logan@users.noreply.github.com>
2022-12-24 18:32:48 +0900

    [chore]: 랜덤 6자리를 대문자로 변경

--- src/util/create-random-password.ts
+++ src/util/create-random-password.ts
@@ -2,6 +2,7 @@ export class RandomGenerator {
   static createRandomString(length: number): string {
     return Math.random()
       .toString(36)
+      .toUpperCase()
       .slice(2, length + 2);
   }
 }
