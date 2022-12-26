c2fa0f0cc32e3d4ee7ae1bdb27ac8029d30ace24
Jeongho Kim <43710866+dev-logan@users.noreply.github.com>
2022-12-26 18:41:32 +0900

    [chore]: cors 설정

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { SWAGGER_CONFIG } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
 
   const document = SwaggerModule.createDocument(app, SWAGGER_CONFIG);
   SwaggerModule.setup('api/docs', app, document);
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
