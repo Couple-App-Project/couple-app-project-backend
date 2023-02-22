02f25db196ad20dee9eaebc277182286a12caba8
Jeongho Kim <43710866+dev-logan@users.noreply.github.com>
2023-02-22 22:32:01 +0900

    [fix] 모듈 providers update

--- src/diaries/diaries.module.ts
+++ src/diaries/diaries.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DiariesController } from './diaries.controller';
 import { DiariesService } from './diaries.service';
 import { CouplesService } from '../couples/couples.service';
+import { CalendarsService } from '../calendars/calendars.service';
 
 @Module({
   controllers: [DiariesController],
-  providers: [DiariesService, CouplesService],
+  providers: [DiariesService, CouplesService, CalendarsService],
 })
 export class DiariesModule {}
